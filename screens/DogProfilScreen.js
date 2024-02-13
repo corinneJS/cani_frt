1c9bbdb0eadfedf9df945f994d0280adeae171eb
corinneJS <corinne.poisson.42@gmail.com>
2024-02-13 10:23:31 +0100

    Ajout Profil Dog : init

--- screens/DogProfilScreen.js
+++ screens/DogProfilScreen.js
@@ -1,6 +1,6 @@
-// RegisterScreen : inscription des utilisateurs
+// DogProfilScreen : Profil d'un 4pattes avec galerie photo, visu caractéristiques races
 // Auteur : CP
-// 
+//
 // --------------------------------------------------
 // import des composants
 import {
@@ -15,72 +15,81 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
+import { DatePickerModal } from "react-native-paper-dates";
 import { LinearGradient } from "expo-linear-gradient";
 // import pour gestion states
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 //CP : import pour la gestion des échanges avec le backend
-import { registerUser_webSrv } from "../webservices/RegisterWebServices.js"; 
+import { dogs_webSrv } from "../webservices/dogs_webSrv.js";
 
 //CP :  import feuille de style globale
 const globalCSS = require("../styles/global.js");
 
-
-
-
-export default function RegisterScreen({ navigation }) {
+export default function DogProfilScreen({ navigation }) {
   const dispatch = useDispatch();
-  const [username, setUsername] = useState("");
-   const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("");
-  const [city, setCity] = useState("");
-  const[isDogOwner, setIsDogOwner] = useState(true);
-  const toggleSwitchDogOwner = ()=> setIsDogOwner(previewsState => !previewsState);
-  const [isProfessional, setIsProfessional] = useState(false);
-  const toggleSwitchProfessional = () => setIsProfessional((previewsState) => !previewsState);
-  
+  const [dogName, setDogName] = useState("");
+  const [birthdate, setBirthdate] = useState(""); // CP A faire DatePicker
+  const [gender, setGender] = useState(""); // CP A faire proposer Mâle / Femelle
+  const [isSterilized, setIsSterilized] = useState(False);
+  const toggleSwitchIsSterilized = () =>
+    setIsSterilized((previewsState) => !previewsState);
+  const [trait, setTrait] = useState(""); // CP : A faire : proposer liste déroulante choix multiple
+  const [activity, setActivity] = useState("");
+  const [dateCreated, setDateCreated] = useState("");
+  const [dateModified, setDateModified] = useState("");
 
-// fct btn connect via backend
-  const handleRegister = async () => { // KB :voir si async est nécessaire risque pb aleatoire
-    const userData = {
-      username: username,
-      email: email,
-      password: password,
-      isDogOwner: isDogOwner,
-      isProfessional: isProfessional,
-      city:city,
+  const [userID, setUserID] = useState(""); // CP : A faire : init avec user connecté
+  const [breedID, setBreedID] = useState("");
+
+  const [openModal, setOpenModal] = React.useState(false);
+  // fct btn Save via backend
+  const handleSave = async () => {
+    const dogData = {
+      dogName: dogName,
+      description: description,
+      birthdate: birthdate,
+      gender: gender,
+      isSterilized: isSterilized,
+      trait: trait,
+      activity: activity,
+      dateCreated: dateCreated,
+      dateModified: dateModified,
+      //userID
+      //breedID
     };
-    console.log(userData);
-    registerUser_webSrv(userData).then((data) => {
+    console.log("dog_data en partance vers ws:", dogData);
+    dogs_webSrv(dogData).then((data) => {
       console.log("data in screen", data);
       if (data.result) {
         dispatch(
-          infoUser({
-            username,
-            email,
-            isDogOwner,
-            isProfessional,
-            city,
-            token: data.token,
+          infoDog({
+            dogName,
+            gender,
+            isSterilized,
           })
         );
-        setUsername("");
-        setEmail("");
-        setIsDogOwner(true);
-        setIsProfessional(false);
-        setPassword("");
-        setCity("");
-        setDescription("");
-        navigation.navigate("TabNavigator");
+        Alert.alert('Profil', `le profil de ${dogName} est enregistré !`)  
+        
       } else {
         Alert.alert("Oups !", `un pb est survenu : ${data.error}`);
       }
     });
-        
   };
 
- 
- 
+  const onDismissSingle =
+    (() => {
+      setOpenModal(false);
+    },
+    [setOpenModal]);
+
+  const onConfirmSingle =
+    ((params) => {
+      setOpenModal(false);
+      setBirthdate(params.birthdate);
+    },
+    [setOpen, setDate]);
+
   return (
     <LinearGradient
       colors={["#F2B872", "#FFFFFF"]}
@@ -90,20 +99,33 @@ export default function RegisterScreen({ navigation }) {
         style={globalCSS.container}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <Image
-          style={styles.image}
-          source={require("../assets/ccLogoColor.png")}
-        />
-        <Text style={globalCSS.stitle}>Bienvenue !</Text>
         <View style={styles.formContent}>
           <TextInput
-            placeholder="nom d'utilisateur"
+            placeholder="nom du 4 pattes"
             label="Choisissez votre :"
-            onChangeText={(value) => setUsername(value)}
-            value={username}
+            onChangeText={(value) => setName(value)}
+            value={name}
             style={globalCSS.input}
           />
-
+          <View
+            style={{ justifyContent: "center", flex: 1, alignItems: "center" }}
+          >
+            <Button
+              onPress={() => setOpen(true)}
+              uppercase={false}
+              mode="outlined"
+            >
+              Choisir une date
+            </Button>
+            <DatePickerModal
+              locale="fr"
+              mode="single"
+              visible={open}
+              onDismiss={onDismissSingle}
+              date={date}
+              onConfirm={onConfirmSingle}
+            />
+          </View>
           <TextInput
             placeholder="votre ville"
             onChangeText={(value) => setCity(value)}
@@ -148,19 +170,19 @@ export default function RegisterScreen({ navigation }) {
             secureTextEntry={true}
             style={globalCSS.input}
           />
-          
+
           <TouchableOpacity
-            onPress={() => handleRegister()}
+            onPress={() => handleSave()}
             style={globalCSS.button}
             activeOpacity={0.8}
           >
-            <Text style={globalCSS.textButton}>je valide</Text>
+            <Text style={globalCSS.textButton}>Enregistrer</Text>
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
     </LinearGradient>
   );
-};
+}
 
 const styles = StyleSheet.create({
   image: {
@@ -185,4 +207,3 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
