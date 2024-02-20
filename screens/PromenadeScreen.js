c0549da479b959eaa342cda448ffee8da8e1253f
Kévin Bah <kevinbah007@gmail.com>
2024-02-20 01:26:17 +0100

    suppression du centrage de la carte qui ne fonctionne pas

--- screens/PromenadeScreen.js
+++ screens/PromenadeScreen.js
@@ -140,14 +140,7 @@ export default function PromenadeScreen() {
       style={globalCSS.backgrdContainer}
     >
       <Text>Welcome to caniconnect PromenadeScreen !</Text>
-      <MapView onLongPress={(e) => handleLongPress(e)} mapType="standard" style={styles.map} 
-          initialRegion={{
-          latitude: 37.4220936,
-          longitude: -122.083922,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
+      <MapView onLongPress={(e) => handleLongPress(e)} mapType="standard" style={styles.map} >
         {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#fecb2d" />}
         {markers}
       </MapView>
