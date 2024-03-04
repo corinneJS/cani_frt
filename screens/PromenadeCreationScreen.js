f5518eed21e154a6a81240364560cfc488659125
Kévin Bah <kevinbah007@gmail.com>
2024-03-04 21:27:29 +0100

    modif de la pop up pour promenade sans marqueur

--- screens/PromenadeCreationScreen.js
+++ screens/PromenadeCreationScreen.js
@@ -72,7 +72,7 @@ export default function PromenadeCreationScreen ({ navigation }) {
   const handleNewWalk= () => {
     // On s'assure que l'utilisateur a placé au moins un marquer
     if (itinerary === undefined || itinerary.length == 0 ) {
-      Alert.alert("Indiquer l'itinéraire de la promenade sur la carte à l'aide de marqueurs, en effectuant un appui long à l'endroit où vous souhaitez placer le marqueur");
+      Alert.alert("Indiquer l'itinéraire en effectuant des appuis longs pour placer des marqueurs");
       return;
     } else {
       // Send new walk to backend to register it in database
